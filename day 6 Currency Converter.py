6dadd080eaaa347e2470b42e885dbf9e96e4d347
akshita-28 <SRIVASTAVAAKSHITA206@GMAIL.COM>
2026-06-25 14:55:14 +0530

    Add files via upload

---
+++ day 6 Currency Converter.py
@@ -0,0 +1,10 @@
+print("="*40)
+print(" CURRENCY CONVERTER")
+print("=" * 40)
+inr =float(input("Enter Amount in INR:"))
+usd =inr/86
+print("\n")
+print("Amount in INR:",inr)
+print("Amount in USD:",round (usd,2))
+
+
