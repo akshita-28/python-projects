c86dc3a5c39a08bee87a63a4b6e27a58b20f3ccb
akshita-28 <SRIVASTAVAAKSHITA206@GMAIL.COM>
2026-06-26 11:37:49 +0530

    AGE CALCULATOR

---
+++ day 7 Age Calculator.py
@@ -0,0 +1,10 @@
+print("=" * 40)
+print("   AGE CALCULATOR")
+print("=" * 40)
+current_year =2026
+birth_year = int(input("Enter your Birth Year:"))
+age=current_year-birth_year
+print("\n")
+print("Current_year:",current_year)
+print("Birth_year:",birth_year)
+print("Your Age is:",age,"years")
