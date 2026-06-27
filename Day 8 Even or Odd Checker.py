cf5c642f20a90ea6c7bb54424593bcda8f35c862
akshita-28 <SRIVASTAVAAKSHITA206@GMAIL.COM>
2026-06-27 10:17:48 +0530

    Delete Day 8 Even or Odd Checker.py

--- Day 8 Even or Odd Checker.py
+++
@@ -1,8 +0,0 @@
-print("=" *40)
-print("   Even or Odd CHECKER")
-print("="*40)
-number = int(input("Enter a number:"))
-if number % 2 == 0:
-    print(number,"is an Even number!!")
-else:
-    print(number,"is an Odd number!!")   
