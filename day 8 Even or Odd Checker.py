e5f79bae1b3348e56de7114b4cce7c5ede5b6c76
akshita-28 <SRIVASTAVAAKSHITA206@GMAIL.COM>
2026-06-27 10:18:24 +0530

    EVEN OR ODD CHECKER

---
+++ day 8 Even or Odd Checker.py
@@ -0,0 +1,8 @@
+print("=" *40)
+print("   Even or Odd CHECKER")
+print("="*40)
+number = int(input("Enter a number:"))
+if number % 2 == 0:
+    print(number,"is an Even number!!")
+else:
+    print(number,"is an Odd number!!")   
