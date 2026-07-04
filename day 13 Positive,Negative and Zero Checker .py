6c2ce7a7173b410088b645f55eb1c82b79408c1f
akshita-28 <SRIVASTAVAAKSHITA206@GMAIL.COM>
2026-07-04 09:23:15 +0530

    Number checker

---
+++ day 13 Positive,Negative and Zero Checker .py
@@ -0,0 +1,11 @@
+print("="*45)
+print("POSITIVE, NEGATIVE, ZERO CHECKER")
+print("="*45)
+number=float(input("Enter a Number:"))
+print()
+if number > 0:
+    print(number,"is a Positive Number")
+elif number<0:
+    print(number,"is a Negative Number") 
+else:
+    print("The Number is zero(0)")       
