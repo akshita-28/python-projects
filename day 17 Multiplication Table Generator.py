37b243cec010f81b10450ba7756cabf34f3bbb5f
akshita-28 <SRIVASTAVAAKSHITA206@GMAIL.COM>
2026-07-06 20:32:07 +0530

    Multiplication Table

---
+++ day 17 Multiplication Table Generator.py
@@ -0,0 +1,9 @@
+print("="*45)
+print("MULTIPLICATION  TABLE GENERATOR")
+print("="*45)
+number=int(input("Enter a Number:"))
+print()
+for i in range (1,11):
+    print(number,"x",i,"=",number*i)
+print()
+print("Table Generated Successfully!")    
