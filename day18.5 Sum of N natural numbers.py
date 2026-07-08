924279b0847ac4981dbf4e4c34f53f23551ca2ba
akshita-28 <SRIVASTAVAAKSHITA206@GMAIL.COM>
2026-07-08 22:17:17 +0530

    Delete day18.5 Sum of N natural numbers.py

--- day18.5 Sum of N natural numbers.py
+++
@@ -1,9 +0,0 @@
-print("="*45)
-print("SUM OF FIRST N NATURAL NUMBERS")
-print("="*45)
-number=int(input("Enter a Number:"))
-sum=0
-for i in range (1,number+1):
-    sum=sum + i
-print()
-print("Sum of First",number,"Natural Numbers=",sum)    
